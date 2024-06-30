276cbccfb3a284faad2d6d815916c0c945ab952c
KronexTechnology <kronextechnology@gmail.com>
2024-06-30 14:44:33 +0530

    changes in manithings
    add new HTML portfolio-v3

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.portfolio__slider-4', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
